04db6788220972ac18c4d77a71fb2c9bd1ba9a04
appcml <35844750+appcml@users.noreply.github.com>
2026-04-30 10:31:51 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -487,7 +487,7 @@ async function sincronizarFavoritos() {
       canal_cat:  ch.cat,
       canal_co:   ch.co,
       canal_logo: ch.logo || '',
-    });
+    }, 'user_id,canal_id');
   }
 
   // 2. Descargar favoritos de la nube
@@ -534,7 +534,7 @@ async function subirFavorito(ch, agregar) {
       canal_cat:  ch.cat,
       canal_co:   ch.co,
       canal_logo: ch.logo || '',
-    });
+    }, 'user_id,canal_id');
   } else {
     await sbDelete('/rest/v1/favoritos?canal_id=eq.' + ch.id + '&user_id=eq.' + sbUser.id);
   }
